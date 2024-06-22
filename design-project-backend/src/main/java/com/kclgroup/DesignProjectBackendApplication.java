e25bf3028dc7b443489d599f827e8f1ad719f9e3
TV233 <TV233@vip.qq.com>
2024-06-22 19:02:27 +0800

    first commit

---
+++ design-project-backend/src/main/java/com/kclgroup/DesignProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kclgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignProjectBackendApplication.class, args);
+	}
+
+}
---
+++ design-project-backend/src/test/java/com/kclgroup/DesignProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kclgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
