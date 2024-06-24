fb910facc2bda9bb2c00dc44fdb3cbf33d56c26a
TV233 <TV233@vip.qq.com>
2024-06-24 21:26:31 +0800

    后端数据overview功能实现

--- design-project-backend/src/main/java/com/kclgroup/backend/controller/OverviewController.java
+++ design-project-backend/src/main/java/com/kclgroup/backend/controller/OverviewController.java
@@ -1,13 +1,14 @@
 package com.kclgroup.backend.controller;
 
-import com.kclgroup.backend.pojo.entity.Result;
-import com.kclgroup.backend.pojo.entity.TopText;
-import com.kclgroup.backend.service.TopTextService;
+import com.kclgroup.backend.pojo.entity.*;
+import com.kclgroup.backend.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -15,6 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class OverviewController {
     @Autowired
     private TopTextService topTextService;
+    @Autowired
+    private EmotionIndexService emotionIndexService;
+    @Autowired
+    private SectorsAndStocksService sectorsAndStocksService;
+    @Autowired
+    private MarketStyleService marketStyleService;
+    @Autowired
+    private PredictService predictService;
     @GetMapping
     public String overview() {
         return "overview";
@@ -24,5 +33,27 @@ public class OverviewController {
         TopText topText = topTextService.getTopText();
         return Result.success(topText);
     }
+    @GetMapping("/emotion_index")
+    public Result<EmotionIndex> emotionIndex() {
+        EmotionIndex emotionIndex = emotionIndexService.getEmotionIndex();
+        return Result.success(emotionIndex);
+    }
+    @GetMapping("/sectors")
+    public Result<List<SectorsAndStocks>> sectors() {
+        List<SectorsAndStocks> sectorsAndStocks = sectorsAndStocksService.getSectorsAndStocks();
+        return Result.success(sectorsAndStocks);
+    }
+    @GetMapping("/market_style")
+    public Result<List<MarketStyle>> marketStyle() {
+        List<MarketStyle> marketStyles = marketStyleService.getMarketStyle();
+        return Result.success(marketStyles);
+    }
+
+    @GetMapping("/predict")
+    public Result<List<Predict>> predict() {
+        List<Predict> predicts = predictService.getPredict();
+        return Result.success(predicts);
+    }
+
 
 }
